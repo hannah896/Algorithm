852a668953a6191e043e33e20f179456edd0eb0d
hannah896 <phn2260@naver.com>
2025-04-16 23:16:48 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 5.25 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cs
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cs
@@ -1,16 +1,14 @@
 using System;
 
-public class Solution
-{
-    public int solution(string myString, string pat)
-    {
-        if (myString.ToLower().IndexOf(pat.ToLower()) > -1)
+public class Solution {
+    public int solution(string myString, string pat) {
+        if(myString.ToLower.indexof(pat.ToLower) > -1)  
+        {
+            return 0; 
+        }
+        else 
         {
             return 1;
-        }
-        else
-        {
-            return 0;
         }
     }
 }

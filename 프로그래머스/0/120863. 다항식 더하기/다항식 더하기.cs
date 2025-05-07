dd93fd548ed1af06b8e1ad05ceb18eaa388604ec
hannah896 <phn2260@naver.com>
2025-05-08 00:48:29 +0900

    [level 0] Title: 다항식 더하기, Time: 1.21 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.cs
@@ -0,0 +1,83 @@
+using System;
+
+public class Solution
+{
+    private int x = 0;
+    private int defalut = 0;
+    private void AddValue(string v)
+    {
+        if (v.Contains("x"))
+        {
+            string n = v.Split('x')[0];
+            if (int.TryParse(n, out int num))
+            {
+                x += num;
+            }
+            else
+            {
+                x += 1;
+            }
+            
+        }
+        else
+        {
+            defalut += int.Parse(v);
+        }
+    }
+    public string solution(string polynomial)
+    {
+        polynomial = polynomial.Trim();
+        string[] s = polynomial.Split("+");
+        
+        foreach (string v in s)
+        {
+            AddValue(v);
+        }
+        string answer = string.Empty;
+
+        //0인경우
+        if (x == 0 && defalut == 0)
+        {
+            return "0";
+        }
+        //x처리
+        if (x != 0)
+        {
+            if (x == 1)
+            {
+                answer += "x";
+            }
+            else
+            {
+                answer += $"{x}x";
+            }   
+        }
+        //실수 처리
+        if (defalut != 0)
+        {
+            if (defalut < 0)
+            {
+                if (x != 0)
+                {
+                    answer += $" - {defalut}";
+                }
+                else
+                {
+                    answer += $"- {defalut}";
+                }
+            }
+            else
+            {
+                if (x != 0)
+                {
+                    answer += $" + {defalut}";
+                }
+                else
+                {
+                    answer += $"{defalut}";
+                }
+            }
+        }
+        return answer;
+    }
+}

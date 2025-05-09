55ffd118bfeb544f174754bc1a17c60b4a936485
hannah896 <phn2260@naver.com>
2025-05-09 21:54:04 +0900

    [level 3] Title: 숫자 게임, Time: 34.59 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.cs
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.cs
@@ -1,35 +1,8 @@
 using System;
 
-public class Solution
-{
-    public int solution(int[] A, int[] B)
-    {
-        Array.Sort(A);
-        Array.Reverse(A);
-        Array.Sort(B);
-        Array.Reverse(B);
-
-        int answer = 0;
-        int n = A.Length;
-
-        int idxB = 0;
-        int usedIdx = n - 1;
-
-        
-        for (int i = 0; i < n; i++)
-        {
-            if (A[i] >= B[idxB])
-            {
-                usedIdx -= 1;
-                continue;
-            }
-            else
-            {
-                idxB++;
-                answer++;
-            }
-        }
-        
+public class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = -1;
         return answer;
     }
 }

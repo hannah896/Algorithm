36090a5cf89a9974e98eeb78f5cf236c6dbc2778
hannah896 <phn2260@naver.com>
2025-06-12 15:17:07 +0900

    [level 2] Title: 완전범죄, Time: 3.07 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Solution
+{
+    int answer = Int32.MaxValue;
+
+    HashSet<(int, int, int)> visited = new HashSet<(int, int, int)>();
+
+    int N, M;
+
+    public void DFS(int[,] info, int A = 0, int B = 0, int idx = 0)
+    {
+        if (A >= answer || visited.Contains((A, B, idx)))
+            return;
+
+        visited.Add((A, B, idx));
+
+        if (idx == info.GetLength(0))
+        {
+            answer = Math.Min(answer, A);
+            return;
+        }
+
+        bool CanA = A + info[idx, 0] < N;
+        bool CanB = B + info[idx, 1] < M;
+
+        if (CanB)
+            DFS(info, A, B + info[idx, 1], idx + 1);
+        if (CanA)
+            DFS(info, A + info[idx, 0], B, idx + 1);
+    }
+
+    public int solution(int[,] info, int n, int m)
+    {
+        int size = info.GetLength(0);
+        N = n;
+        M = m;
+
+        DFS(info);
+
+        return answer == Int32.MaxValue ? -1 : answer;
+    }
+
+    // [[1, 2], [2, 3], [2, 1]]
+}

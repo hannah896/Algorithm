1c3e74c32d476a43c9f97c4c4758c7e620e4fe28
hannah896 <phn2260@naver.com>
2025-06-20 01:29:51 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.38 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cs
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cs
@@ -1,9 +1,22 @@
-using System.Linq;
+using System;
+using System.Collections.Generic;
 
-public class Solution
-{
-    public int solution(int[] A, int[] B)
-    {
-        return A.OrderBy(x => x).Zip(B.OrderByDescending(x => x), (a, b) => a * b).Sum();
+public class Solution {
+    public int solution(int[] A, int[] B) {
+       int sum_min = 0;
+
+            List<int> A_temp = new List<int>(A);
+            List<int> B_temp = new List<int>(B);
+
+            A_temp.Sort();
+            B_temp.Sort();
+            B_temp.Reverse();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                sum_min += A_temp[i] * B_temp[i];
+            }
+
+        return sum_min;
     }
 }

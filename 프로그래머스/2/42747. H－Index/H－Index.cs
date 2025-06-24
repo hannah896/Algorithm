f0630ed521ceec989a4febe6a15c3e6c5f943350
hannah896 <phn2260@naver.com>
2025-06-24 21:26:52 +0900

    [level 2] Title: H-Index, Time: 148.30 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cs
+++ 프로그래머스/2/42747. H－Index/H－Index.cs
@@ -1,36 +1,18 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
-public class Solution
-{
-    public int solution(int[] citations)
-    {
-        Array.Sort(citations);
-
-        int ans = 0;
-        int maxVal = citations.Max();
-
-        for (int i = 0; i < maxVal; i++)
+public class Solution {
+            public int solution(int[] citations)
         {
-            int count = 0;
-            foreach(int c in citations)
+            int answer = 0;
+            for (int i = citations.Max(); i > answer; i--)
             {
-                if (c >= i)
+                if (citations.Count(e => e >= i) >= i)
                 {
-                    count++;
+                    answer = i;
                 }
             }
-            if (i <= count)
-            {
-                ans = Math.Max(ans, i);
-            }
-            else
-            {
-                break;
-            }
-        }
 
-        return ans;
-    }
+            return answer;
+        }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 4.35 ms
+메모리: 32.8 MB, 시간: 148.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 24일 21:21:50
+2025년 06월 24일 21:26:50
 
 ### 문제 설명
 

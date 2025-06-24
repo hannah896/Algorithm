e1b5650bf7c7a0d8f70a81d9183cb4bf2be3302d
hannah896 <phn2260@naver.com>
2025-06-24 21:28:05 +0900

    [level 2] Title: H-Index, Time: 5.56 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cs
+++ 프로그래머스/2/42747. H－Index/H－Index.cs
@@ -1,18 +1,41 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
-
-public class Solution {
-            public int solution(int[] citations)
+public class Solution
+    {
+        List<int> notes;
+        int max = 0;
+        public int solution(int[] citations)
         {
-            int answer = 0;
-            for (int i = citations.Max(); i > answer; i--)
+            notes = new List<int>(citations);
+            notes.Sort();
+            if (notes[0] >= notes.Count)
+                return notes.Count;
+            int avg = (int)(notes.Count / 2);
+            int ans = Check(avg);
+            return ans;
+        }
+        private int Check(int avg)
+        {
+            int higher = 0;
+            foreach(int a in notes)
             {
-                if (citations.Count(e => e >= i) >= i)
+                if( a>= avg)
+                    higher++;
+            }
+            if (max < avg)
+            {
+                if (higher<avg) //to high
                 {
-                    answer = i;
+                    return Check(avg - 1);
+                }
+                else
+                {
+                    max = avg;
+                    return Check(avg + 1);
                 }
             }
-
-            return answer;
+            return max;
         }
-}
+
+    }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 32.8 MB, 시간: 148.30 ms
+메모리: 31.6 MB, 시간: 5.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 24일 21:26:50
+2025년 06월 24일 21:28:03
 
 ### 문제 설명
 

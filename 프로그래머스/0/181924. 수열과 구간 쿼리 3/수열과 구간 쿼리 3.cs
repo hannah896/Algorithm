4694200b5090c834e6f67e8a5b3fc6015fb077ff
hannah896 <phn2260@naver.com>
2025-10-02 16:46:56 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.32 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) 
+    {
+        for (int i = 0; i< queries.GetLength(0); i++)
+        {
+            int idx1 = queries[i,0];
+            int idx2 = queries[i,1];
+            
+            int temp = arr[idx1];
+            arr[idx1] = arr[idx2];
+            arr[idx2] = temp;
+        }
+        return arr;
+    }
+}

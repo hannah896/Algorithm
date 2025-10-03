8a4f4cfef7966238c0439c7f0d22ad7d9d1eba84
hannah896 <phn2260@naver.com>
2025-10-03 17:51:27 +0900

    [level 0] Title: 카운트 업, Time: 0.41 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int start_num, int end_num) 
+    {
+        return Enumerable.Range(start_num, end_num - start_num +1).ToArray();
+    }
+}

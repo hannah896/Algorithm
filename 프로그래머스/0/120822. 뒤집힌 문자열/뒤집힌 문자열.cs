40d05f668992c3b03c24ef89db95c56212bc7dd7
hannah896 <phn2260@naver.com>
2025-10-09 20:19:43 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 2.25 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+public class Solution 
+{
+    public string solution(string my_string) 
+    {
+        return new string(my_string.Reverse().ToArray());
+    }
+}
